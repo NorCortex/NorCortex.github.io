ba0db885d64a53000cee44c81d6041f6dd4b6a16
Chanatip Deemee <78657820+Loxyez@users.noreply.github.com>
2025-02-26 22:45:38 +0700

    Try direct url

--- next.config.ts
+++ next.config.ts
@@ -16,7 +16,7 @@ const nextConfig: NextConfig = {
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
   basePath: isProd ? "/nextjs-github-pages" : "",
-  assetPrefix: isProd ? "/nextjs-github-pages/" : "",
+  assetPrefix: isProd ? "https://norcortex.github.io/nextjs-github-pages/" : "",
 
   /**
    * Disable server-based image optimization. Next.js does not support
